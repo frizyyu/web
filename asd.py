606e29195925ff9a61b4dc91b1c895210bc5756d
Artem <84192047+neneton@users.noreply.github.com>
2022-03-31 16:58:10 +0700

    Update asd.py

--- asd.py
+++ asd.py
@@ -516,7 +516,8 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port)
 
 
 #акк яндекса
